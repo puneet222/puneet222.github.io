4ba771546a9d803d980af94c34f4541f7b08f80e
puneet222 <puneetaggarwal@pnplabs.in>
2017-03-25 16:37:14 +0530

    testing

--- js/adminApp.js
+++ js/adminApp.js
@@ -4,8 +4,13 @@ var portfolio = angular.module('portfolio' , ['ngAnimate',
 .config(function ($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise("/");
   $stateProvider
+  .state('home' , {
+    url : '/',
+      templateUrl: 'index2.html',
+      controller : 'portfolioController.js'
+  })
   .state('temporary' , {
-    url: '/temp',
+    url: 'temp',
     templateUrl : 'temporary.html',
     controller : 'tempController'
   })
