79bf05a99c1696a09d06270b5bb92003f14b50e1
puneet222 <puneetaggarwal@pnplabs.in>
2017-03-25 22:04:37 +0530

    basic info page without video

--- public/js/portfolioController.js
+++ public/js/portfolioController.js
@@ -16,16 +16,12 @@ angular.module('portfolio').controller('portfolioController' , function ($scope)
             scrollingSpeed: 500,
             afterRender : function () {
                 // playing the video
-                $('video').get(0).play();
+               // $('video').get(0).play();
             },
             onLeave: function(index, nextIndex, direction) {
                 if(nextIndex == 2){
-                    $(".skills").addClass("animated fadeInUpBig" , 1000) ;
-                    $(".skills").addClass("sliding-middle-out" , 1000) ;
-                    $(".skills").delay(2500).queue(function (next) {
-                        $(".skills").trigger("hover");
-                        next();
-                    }) ;
+                    $("#skills").addClass("animated fadeInUpBig" , 1000) ;
+                    $("#skills").addClass("sliding-middle-out" , 1000) ;
                 }
             }
         });
