dd474e3e822c5008849a6e751140ee8447173d5c
puneet222 <puneetaggarwal@pnplabs.in>
2017-03-26 23:12:00 +0530

    every thing delete starting from scract

--- public/js/portfolioController.js
+++ public/js/portfolioController.js
@@ -1,10 +1,16 @@
 angular.module('portfolio').controller('portfolioController' , function ($scope) {
 
+
+
         $scope.init = function () {
             $scope.html = 555 ;
             // getProgress() ;
 
+
+
         };
+
+
 
         // function getProgress() {
         //     $('.progress').asProgress('start');
