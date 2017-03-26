491ed9e62f3613080523265962cef30ca908f064
puneet222 <puneetaggarwal@pnplabs.in>
2017-03-26 17:04:53 +0530

    its okay but sometimes get failed

--- public/js/portfolioController.js
+++ public/js/portfolioController.js
@@ -1,31 +1,19 @@
 angular.module('portfolio').controller('portfolioController' , function ($scope) {
 
-    $scope.init = function () {
-        initializePagepiling() ;  // --------------  initializing pagePiling
+        $scope.init = function () {
+            $scope.html = 555 ;
+            // getProgress() ;
 
-        $("#pp-nav").hide() ;   // -----------   removing the tool tip (dots) of pagePiling
+        };
 
-        $('.scrollDown').click(function(){      // ------------   function to scroll down the section of pagePiling
-            $.fn.pagepiling.moveSectionDown();
-        });
-
-    };
-
-    function initializePagepiling() {
-        $('#pagepiling').pagepiling({
-            scrollingSpeed: 500,
-            afterRender : function () {
-                // playing the video
-               // $('video').get(0).play();
-            },
-            onLeave: function(index, nextIndex, direction) {
-                if(nextIndex == 2){
-                    $("#skills").addClass("animated fadeInUpBig" , 1000) ;
-                    $("#skills").addClass("sliding-middle-out" , 1000) ;
-                }
-            }
-        });
-    }
+        // function getProgress() {
+        //     $('.progress').asProgress('start');
+        //     $('.progress').asProgress('done');
+        //     $('.progress').asProgress('go',50);
+        //     $('.progress').asProgress('go','50%');
+        //     $('.progress').asProgress('stop');
+        //     $('.progress').asProgress('reset');
+        // }
 
 
 });
