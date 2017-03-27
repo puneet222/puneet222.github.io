23ec3a270a669afe2b09c6d674c4a47117890cee
puneet222 <puneetaggarwal@pnplabs.in>
2017-03-27 11:15:52 +0530

    two sectoin almost created without video background

--- public/js/portfolioController.js
+++ public/js/portfolioController.js
@@ -1,25 +1,11 @@
 angular.module('portfolio').controller('portfolioController' , function ($scope) {
 
-
-
-        $scope.init = function () {
-            $scope.html = 555 ;
-            // getProgress() ;
-
-
-
-        };
+    $scope.init = function () {
+        $scope.html = 555 ;
+        // getProgress() ;
 
 
 
-        // function getProgress() {
-        //     $('.progress').asProgress('start');
-        //     $('.progress').asProgress('done');
-        //     $('.progress').asProgress('go',50);
-        //     $('.progress').asProgress('go','50%');
-        //     $('.progress').asProgress('stop');
-        //     $('.progress').asProgress('reset');
-        // }
-
+    };
 
 });
